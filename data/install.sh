d9b381f8b3f6f5010312c0ca6d2c8c4819ec2f6e
BitanSarkar007 <82215438+BitanSarkar007@users.noreply.github.com>
2024-04-10 20:17:50 +0530

    Update install.sh

--- data/install.sh
+++ data/install.sh
@@ -10,15 +10,18 @@ curl -s https://get.docker.com/ | sh
 echo -e "\e[1;31m  Run docker service \e[0m" 
 systemctl start docker
 
-echo -e "\e[1;31m Clone VulHub \e[0m"
+echo -e "\e[1;31m Clone Vulnerables file \e[0m"
 
 git clone https://github.com/BitanSarkar007/vulhubsh.git
 cd vulhubsh
-git clone https://github.com/BitanSarkar007/vulhub.git
 
 echo -e "\e[1;31m Give executive permission \e[0m"
 
 chmod +x vulnerables.sh
+
+echo -e "\e[1;31m Clone vulhub \e[0m"
+
+git clone https://github.com/BitanSarkar007/vulhub.git
 
 echo -e "\e[1;31m Start vulhub \e[0m"
 
